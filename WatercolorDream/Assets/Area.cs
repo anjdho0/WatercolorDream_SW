7e2cd16cacbfddf1ca2c5b9fa3f65a4bf951557d
17김승우 <anjdho0@gmail.com>
2018-03-26 00:32:07 +0900

    ColorExtension을 MyExtension으로 변경, Mesh로 육각형 그리는 함수 추가. Material-glass임포트.

--- WatercolorDream/Assets/Area.cs
+++ WatercolorDream/Assets/Area.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Area : MonoBehaviour{
+    
+    [SerializeField]
+    MeshFilter meshFilter;
+
+    [SerializeField]
+    MeshRenderer meshRenderer;
+
+    [SerializeField]
+    MeshCollider meshCollider;
+
+    Mesh mesh;
+
+    
+    private void Awake()
+    {
+        mesh = new Mesh();
+        meshFilter.mesh = mesh;
+        mesh.DrawHexagon(gameObject.transform, 5.0f);
+        
+    }
     // Use this for initialization
     void Start()
     {
@@ -13,46 +33,6 @@ public class Area : MonoBehaviour{
     void Update()
     {
         
-    }
-    //[SerializeField]
-    //[SerializeField]
-
-    MeshFilter meshFilter;
-    MeshRenderer meshRenderer;
-    Mesh mesh;
-    float radius = 5.0f;
-    
-    private void Awake()
-    {
-        Vector3[] vertecties = new Vector3[7];
-        int[] tris = new int[18];
-        mesh = new Mesh();
-        meshFilter = gameObject.AddComponent<MeshFilter>();
-        meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshFilter.mesh = mesh;
-        vertecties[0] = new Vector3(0, 0);
-
-        for(int i = 1; i < 7; i++)
-        {
-            vertecties[i] = new Vector3(radius * Mathf.Sin(Mathf.PI / 3 * i), radius * Mathf.Cos(Mathf.PI / 3 * i));
-        }
-        mesh.vertices = vertecties;
-
-        for(int i = 0; i <6; i++)
-        {
-            tris[i * 3] = 0;
-            tris[i * 3 + 1] = i + 1;
-            if (i == 5)
-            {
-                tris[i * 3 + 2] = 1;
-            }
-            else
-            {
-                tris[i * 3 + 2] = i + 2;
-            }
-
-        }
-        mesh.triangles = tris;
     }
 
 }
