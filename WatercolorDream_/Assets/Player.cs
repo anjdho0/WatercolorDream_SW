ebe0c5b10ba30528edba52ec9e8077dea3188246
17김승우 <anjdho0@gmail.com>
2018-03-31 13:04:46 +0900

    Player 스크립트 생성.

---
+++ WatercolorDream_/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public CMYK cmyk;
+    public Camera scope;
+    public Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        cmyk = new CMYK(Color.white);
+        scope = GetComponentInChildren<Camera>();
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(gameObject.transform.right.normalized * (-1));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(gameObject.transform.right.normalized);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(gameObject.transform.forward.normalized);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(gameObject.transform.forward.normalized * (-1));
+        }
+	}
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall")) 
+        {
+            rigidbody.AddForce(collision.gameObject.transform.up * 300, ForceMode.Acceleration);
+        }
+    }
+}
