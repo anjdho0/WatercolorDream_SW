68c1f39f70c0f0852af3e2dfbbe21eac8e89b33f
17김승우 <anjdho0@gmail.com>
2018-04-04 14:33:57 +0900

    일단 타일을 넣었음...

---
+++ WatercolorDream_/Assets/Area.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour {
+
+    List<GameObject> Tiles = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<MeshRenderer>().material.color = Color.cyan;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- WatercolorDream_/Assets/Player.cs
+++ WatercolorDream_/Assets/Player.cs
@@ -7,31 +7,33 @@ public class Player : MonoBehaviour {
     public CMYK cmyk;
     public Camera scope;
     public Rigidbody rigidbody;
+    public float speed;
 
 	// Use this for initialization
 	void Start () {
         cmyk = new CMYK(Color.white);
         scope = GetComponentInChildren<Camera>();
         rigidbody = GetComponent<Rigidbody>();
+        speed = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            gameObject.transform.Translate(gameObject.transform.right.normalized * (-1));
+            gameObject.transform.Translate(gameObject.transform.right.normalized * (-1) * speed);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.transform.Translate(gameObject.transform.right.normalized);
+            gameObject.transform.Translate(gameObject.transform.right.normalized * speed);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            gameObject.transform.Translate(gameObject.transform.forward.normalized);
+            gameObject.transform.Translate(gameObject.transform.forward.normalized * speed);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            gameObject.transform.Translate(gameObject.transform.forward.normalized * (-1));
+            gameObject.transform.Translate(gameObject.transform.forward.normalized * (-1) * speed);
         }
 	}
     
