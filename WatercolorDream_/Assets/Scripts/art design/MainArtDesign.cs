7fa7820d90aa611340d35b7cc0ba7e8c816516b2
YangHun <pitahano@gmail.com>
2018-08-27 18:24:46 +0900

    Finish Title art design - add skybox - change file name of cube png images - add camera move using interpolation - add lens flare using shader assets - add fonts - change title from image to text - adjust button color and position

---
+++ WatercolorDream_/Assets/Scripts/art design/MainArtDesign.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainArtDesign : MonoBehaviour {
+
+	Vector3 start;
+	Vector3 end;
+	float timer = 0.0f;
+	const float rotateTime = 10.0f;
+
+	void Start() {
+		start = new Vector3(80.0f, 80.0f, 80.0f);
+		end = start + new Vector3(360.0f, 360.0f, -360.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (timer < rotateTime) {
+
+			timer += Time.deltaTime;
+			Camera.main.transform.rotation = Quaternion.Euler (Vector3.Lerp(start, end, timer / rotateTime));
+
+		}
+		else {
+			timer = 0.0f;
+		}
+
+	}
+}
